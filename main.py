5664d16ff8d183545255d1ae007eb2aac278294d
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-04-07 22:01:09 -0300

    iniciando-arrays

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-
+import numpy as np
 
 def main():
-    print("******************")
+    km = np.loadtxt('data\\carros-km.txt')
+    anos = np.loadtxt('data\\carros-anos.txt')
+    
+    km_media = km / (2022 - anos) #quantos km os carros rodaram em media por ano
+    print(km_media)
 
 
 if(__name__ == "__main__"):
