7772a364d6038553fbea5e3a83f424c77acb5001
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-04-09 22:05:50 -0300

    estudo-09042022

---
+++ testes.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def main():
+    
+    anos = np.loadtxt(fname = "data\\carros-anos.txt", dtype = int)
+    km = np.loadtxt(fname = "data\\carros-km.txt")
+    valor = np.loadtxt(fname = "data\\carros-valor.txt")
+
+
+
+if(__name__ == "__main__"):
+    main()
